5c4ac680c8d767ecc419cb4143d10f9fb4fe1164
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-09-21 15:32:32 +0530

    Modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import ProtectedRoute from './components/protectedRoute';
+import Home from './pages/home';
+import Register from './pages/register';
+import Scrape from './pages/scrape';
+import Generate from './pages/generate';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <ProtectedRoute path='/scrape' element={<Scrape/>}></ProtectedRoute>
+        <ProtectedRoute path='/generate' element={<Generate/>}></ProtectedRoute>
+      </Routes>
     </div>
   );
 }
