dc1d47284abbf8595437eed9c043ed10d65fea7b
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-09-21 23:09:52 +0530

    Modified App.js and added a new both.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import ProtectedRoute from './components/protectedRoute';
+import {Route, Routes} from 'react-router-dom';
 import Home from './pages/home';
 import Register from './pages/register';
 import Scrape from './pages/scrape';
 import Generate from './pages/generate';
+import Both from './pages/both';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/register' element={<Register/>}></Route>
-          <ProtectedRoute path='/scrape' element={<Scrape/>}></ProtectedRoute>
-          <ProtectedRoute path='/generate' element={<Generate/>}></ProtectedRoute>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  if (!sessionStorage.getItem('isAuthenticated')){
+    return (
+      <div className="App">
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/register' element={<Register/>}></Route>
+          </Routes>
+      </div>
+    );
+  }else{
+    return(
+      <div className="App">
+          <Routes>
+            <Route path='/' element={<Scrape/>}></Route>
+            <Route path='/generate' element={<Generate/>}></Route>
+            <Route path='/both' element={<Both/>}></Route>
+          </Routes>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ client/src/pages/both.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Both = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default Both;
