12cc37451c2aba38b1a3e9b71a1f6c801137728f
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-09-23 12:08:19 +0530

    Modified Navbar so that it stays at top

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Navbar = () => {
   return(
-    <nav class="navbar" style={{background: '#004225'}}>
+    <nav class="navbar fixed-top" style={{background: '#004225'}}>
       <form class="container-fluid justify-content-start">
         <a class="btn me-2" style={{background: '#F5F5DC', color: '#004225'}} type="button" href="/">Home</a>
         <a class="btn me-2" style={{background: '#F5F5DC', color: '#004225'}} type="button" href="/register">Register</a>
