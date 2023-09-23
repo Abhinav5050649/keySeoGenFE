5803aa33616e212a8d5fa87306478458a604f3f3
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-09-23 12:07:36 +0530

    Added form to register page, modified home page a bit

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,6 +5,10 @@ export const Home = () => {
     return (
         <>
             <Navbar/>
+            <div className="container">
+                <h1 style={{marginTop: '15%', marginBottom: '1%', textAlign: 'center', fontFamily: 'serif', fontSize: '5rem', color: '#004225'}}><strong>primeGen</strong></h1>
+                <blockquote style={{fontSize: '2rem', color: '#004225'}}>All your SEO Needs, at one place!</blockquote>
+            </div>
         </>
     );
 }
